39501fb70fffe657eef3d19ac9babc2b2f45a4db
nunor <nunor@LAPTOP-DCPD1A9G>
2019-12-19 16:15:57 +0000

    commeting services

--- pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/service/ClassSearchServices.java
+++ pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/service/ClassSearchServices.java
@@ -3,13 +3,13 @@ package pt.iscte.pidesco.search.service;
 public interface ClassSearchServices {
 
 	/**
-	 * 
+	 * Not implemented
 	 * @return number of matches of the found word
 	 */
 	int numberOfMatches();
 	
 	/**
-	 * 
+	 * Not implemented
 	 * @param word -> the string you require to search (it has to be a complete word)
 	 * @param options -> set which javaTypes you want to search [variables, methods, fields]
 	 */
